83d6435cb828984f6d1fac1bfe91f0369eee05b2
phuchoang2411 <phuchoang2411@gmail.com>
2020-11-20 22:47:25 +0700

    add host

--- dictionary/Database.cs
+++ dictionary/Database.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace dictionary
 {
     static class Database
     {
-        private static int now;
+        private static  int now;
+        public static string connString = @"DESKTOP-I4FVPU0\E:\Data.sql;Database=Data;User Id=sa;Password=123456789;";
+        public static void connect()
+        {
+            SqlConnection connection = new SqlConnection(connString);
+            connection.Open();
+            string sqlQuery = "SELECT * FROM Data WHERE English=";
+            SqlCommand command = new SqlCommand(sqlQuery, connection);
+            SqlDataReader reader = command.ExecuteReader();
+        }      
     }
 }
